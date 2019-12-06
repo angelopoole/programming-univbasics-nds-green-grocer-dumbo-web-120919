9afaa38cd494787323b066ebebf1f7dfd841618f
Angelo Poole <angelo.poole@outlook.com>
2019-12-06 11:40:26 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -9,7 +9,7 @@
 #   end
 #   nil
 # end
-# 
+#
 # def consolidate_cart(cart)
 #   # Consult README for inputs and outputs
 #   #
@@ -17,7 +17,7 @@
 #   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
 #   i = 0
 #   result = []
-# 
+#
 #   while i < cart.count do
 #     item_name = cart[i][:item]
 #     sought_item = find_item_by_name_in_collection(item_name, result)
@@ -29,10 +29,10 @@
 #     end
 #     i += 1
 #   end
-# 
+#
 #   result
 # end
-# 
+#
 # def apply_coupons(cart, coupons)
 #   # Consult README for inputs and outputs
 #   #
@@ -43,16 +43,16 @@
 #     item_with_coupon = find_item_by_name_in_collection(coupon[:item], cart)
 #     item_is_in_basket = !!item_with_coupon
 #     count_is_big_enough_to_apply = item_is_in_basket && item_with_coupon[:count] >= coupon[:num]
-# 
+#
 #     if item_is_in_basket and count_is_big_enough_to_apply
 #       apply_coupon_to_cart(item_with_coupon, coupon, cart)
 #     end
 #     i += 1
 #   end
-# 
+#
 #   cart
 # end
-# 
+#
 # def apply_clearance(cart)
 #   # Consult README for inputs and outputs
 #   #
@@ -66,10 +66,10 @@
 #   end
 #   i += 1
 # end
-# 
+#
 # cart
 # end
-# 
+#
 # def checkout(cart, coupons)
 #   # Consult README for inputs and outputs
 #   #
@@ -82,16 +82,16 @@
 #   # some irritated customers
 #   total = 0
 # i = 0
-# 
+#
 # ccart = consolidate_cart(cart)
 # apply_coupons(ccart, coupons)
 # apply_clearance(ccart)
-# 
+#
 # while i < ccart.length do
 #   total += items_total_cost(ccart[i])
 #   i += 1
 # end
-# 
+#
 # total >= 100 ? total * (1.0 - BIG_PURCHASE_DISCOUNT_RATE) : total
 # end
 
